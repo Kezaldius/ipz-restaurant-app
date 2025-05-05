2cd68710d3b331bc8a05233d3fa8adc531d0a73a
Kezaldius <132710860+Kezaldius@users.noreply.github.com>
2025-05-05 22:15:15 +0300

    Fixed CORS policy errors

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -9,6 +10,7 @@ migrate = Migrate()
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    CORS(app)
     db.init_app(app)
     migrate.init_app(app, db)
 
